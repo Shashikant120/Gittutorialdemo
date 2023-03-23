cf63d6245ff20867a981301fbd30e9fa05181aba
Shashikant120 <128563439+Shashikant120@users.noreply.github.com>
2023-03-23 17:42:10 +0530

    GetElementbytagname

---
+++ GetElementsbyTAgName.js
@@ -0,0 +1,16 @@
+//GetElementbyclassName use for newly added element
+let newElement = document.getElementsByClassName("Get-Element-by-tag-name");
+console.log(newElement);
+newElement[0].style.color ="red";
+newElement[0].style.backgroundColor="Cyan";
+newElement[0].style.fontWeight="bold";
+
+
+//Get Element by tagname
+
+let tagelement = document.getElementsByTagName("li");
+console.log(tagelement);
+tagelement[4].style.fontFamily="timesnewroman";
+tagelement[4].style.textAlign="center";
+
+
